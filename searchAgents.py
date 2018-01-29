2a0d6bde993288e8a47793c99e3d8383cbbfc341
Andrew Bin Haji <leet-shadow@hotmail.com>
2018-01-28 22:02:51 -0500

    Update searchAgents.py

--- searchAgents.py
+++ searchAgents.py
@@ -308,11 +308,11 @@ class CornersProblem(search.SearchProblem):
         Stores the walls, pacman's starting position and corners.
         """
         self.walls = startingGameState.getWalls()
-        print(self.walls)
+        #print(self.walls)
         self.startingPosition = startingGameState.getPacmanPosition()
-        print(self.startingPosition)
+        #print(self.startingPosition)
         top, right = self.walls.height-2, self.walls.width-2
-        print(top,right)
+        #print(top,right)
         self.corners = ((1,1), (1,top), (right, 1), (right, top))
         for corner in self.corners:
             if not startingGameState.hasFood(*corner):
@@ -495,34 +495,38 @@ class AStarFoodSearchAgent(SearchAgent):
 
 def foodHeuristic(state, problem):
     """Your heuristic for the FoodSearchProblem goes here.
-
     This heuristic must be admissible to ensure correctness.
-
     If using A* ever finds a solution that is worse uniform cost
     search finds, your heuristic is *not* admissible!  On the other
     hand, inadmissible heuristics may occasionally find optimal
     solutions, so be careful.
-
     The state is a tuple ( pacmanPosition, foodGrid ) where foodGrid is a Grid
     (see game.py) of either True or False. You can call foodGrid.asList() to get
     a list of food coordinates instead.
-
     If you want access to info like walls, capsules, etc., you can query the
     problem.  For example, problem.walls gives you a Grid of where the walls
     are.
-
     If you want to *store* information to be reused in other calls to the
     heuristic, there is a dictionary called problem.heuristicInfo that you can
     use. For example, if you only want to count the walls once and store that
     value, try: problem.heuristicInfo['wallCount'] = problem.walls.count()
     Subsequent calls to this heuristic can access
     problem.heuristicInfo['wallCount']
-
     """
     position, foodGrid = state
-    print "position",position,"foodgrid\n",foodGrid.asList()
     "*** YOUR CODE HERE ***"
-    return 0
+    
+    if (foodGrid.asList()):
+        m = 0
+        
+        for xy in foodGrid.asList():
+            distance = mazeDistance(xy, position, problem.startingGameState)
+            if distance > m:
+                m = distance
+                
+        return m
+    else:
+        return 0
 
 class ClosestDotSearchAgent(SearchAgent):
     "Search for all food using a sequence of searches"
